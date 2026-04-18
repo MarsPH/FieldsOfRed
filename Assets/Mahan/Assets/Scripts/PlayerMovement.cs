8fac0a0d3a107080d26fbe093120a25a0b9bd83a
MarsPH <m.poorhamidian@gmail.com>
2026-04-17 20:46:28 -0400

    Added:PlayerMovement + basic protoyype material

--- Assets/Mahan/Assets/Scripts/PlayerMovement.cs
+++ Assets/Mahan/Assets/Scripts/PlayerMovement.cs
@@ -3,30 +3,25 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    private float inputX;
-    private float inputY;
-
-    public float walkSpeed = 3.0f;
-    public float runSpeed = 6.0f;
+    public float walkSpeed = 3f;
+    public float runSpeed = 6f;
+    public float jumpSpeed = 8f;
+    public float gravity = 20f;
     public bool limitDiagonalSpeed = true;
     public bool toggleRun = false;
-    public float jumpSpeed = 8.0f;
-    public float defGravity = 20.0f;
     public bool airControl = false;
 
-    public GameObject neckJoint;
-
-    private float antiBumpFactor = .75f;
+    private float inputX;
+    private float inputY;
+    private float speed;
+    private float antiBumpFactor = 0.75f;
     private int antiBunnyHopFactor = 1;
+    private int jumpTimer;
+    private bool playerControl;
+    private Vector3 moveDirection = Vector3.zero;
 
-    private bool grounded;
-    private bool running;
     private CharacterController controller;
     private Transform myTransform;
-    private Vector3 moveDirection = Vector3.zero;
-    private float speed;
-    private int jumpTimer;
-    private bool playerControl = false;
 
     void Start()
     {
@@ -43,45 +38,23 @@ public class PlayerMovement : MonoBehaviour
 
         if (toggleRun)
         {
-            if (Input.GetButtonDown("Run"))
+            if (Input.GetKeyDown(KeyCode.LeftShift))
             {
-                speed = (speed == walkSpeed ? runSpeed : walkSpeed);
+                speed = (speed == walkSpeed) ? runSpeed : walkSpeed;
             }
         }
         else
         {
-            speed = Input.GetButton("Run") ? runSpeed : walkSpeed;
+            speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
         }
     }
 
     void FixedUpdate()
     {
-        float inputModifyFactor = (inputX != 0.0f && inputY != 0.0f && limitDiagonalSpeed) ? .7071f : 1.0f;
+        float inputModifyFactor =
+            (inputX != 0f && inputY != 0f && limitDiagonalSpeed) ? 0.7071f : 1f;
 
-        float maxRot = 1.5f;
-        float rate = 2.0f;
-        float currentAngle = neckJoint.transform.rotation.eulerAngles.y;
-
-        if (speed > walkSpeed)
-        {
-            running = true;
-            neckJoint.transform.rotation = Quaternion.Lerp(
-                neckJoint.transform.rotation,
-                Quaternion.Euler(maxRot * 2, currentAngle, 0),
-                Time.deltaTime * rate
-            );
-        }
-        else
-        {
-            running = false;
-            neckJoint.transform.rotation = Quaternion.Lerp(
-                neckJoint.transform.rotation,
-                Quaternion.Euler(inputY * maxRot * 0.1f, currentAngle, -inputX * maxRot * 0.5f),
-                Time.deltaTime * rate
-            );
-        }
-
-        if (grounded)
+        if (controller.isGrounded)
         {
             moveDirection = new Vector3(inputX * inputModifyFactor, -antiBumpFactor, inputY * inputModifyFactor);
             moveDirection = myTransform.TransformDirection(moveDirection) * speed;
@@ -107,7 +80,7 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
-        moveDirection.y -= defGravity * Time.deltaTime;
-        grounded = (controller.Move(moveDirection * Time.deltaTime) & CollisionFlags.Below) != 0;
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
